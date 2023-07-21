911734ebb67ef208f9cec5dc06a2eb0d4c17766c
SSA <ssa@rio-soft.ru>
2023-07-21 12:11:39 +0500

    Исправление работы исключения в тестовой консоли

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,42 +9,33 @@ public class MainClass
         Console.WriteLine("1. Круг\n2. Треугольник\n3. Выход");
         IGeometryFigure? figure = null;
 
-        try
+        string input = Console.ReadLine();
+        bool isValid = int.TryParse(input, out int choice);
+        if (isValid)
         {
-            string input = Console.ReadLine();
-            bool isValid = int.TryParse(input, out int choice);
-            if (isValid)
+            switch (choice)
             {
-                switch (choice)
-                {
-                    case 1:
-                        Console.WriteLine("Введите радиус круга");
-                        int radius = Int32.Parse(Console.ReadLine());
-                        figure = new Circle(radius);
-                        break;
-                    case 2:
-                        Console.WriteLine("Введите стороны треугольника");
-                        double a = Double.Parse(Console.ReadLine());
-                        double b = Double.Parse(Console.ReadLine());
-                        double c = Double.Parse(Console.ReadLine());
-                        figure = new Triangle(a, b, c);
-                        break;
-                    case 3:
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("Неверное значение");
-                        break;
-                }
-                Console.WriteLine("Площадь фигуры " + Convert.ToString(figure?.GetSquare()));
+                case 1:
+                    Console.WriteLine("Введите радиус круга");
+                    int radius = Int32.Parse(Console.ReadLine());
+                    figure = new Circle(radius);
+                    break;
+                case 2:
+                    Console.WriteLine("Введите стороны треугольника");
+                    double a = Double.Parse(Console.ReadLine());
+                    double b = Double.Parse(Console.ReadLine());
+                    double c = Double.Parse(Console.ReadLine());
+                    figure = new Triangle(a, b, c);
+                    break;
+                case 3:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Неверное значение");
+                    break;
             }
+            Console.WriteLine("Площадь фигуры " + Convert.ToString(figure?.GetSquare()));
         }
-        catch(Exception ex)
-        {
-            Console.WriteLine("Ошибка! Входные данные имели неверный формат");
-        }
-
-        
 
     }
 }
